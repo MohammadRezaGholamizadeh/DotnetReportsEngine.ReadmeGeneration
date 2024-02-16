35b54b6ec9ab1c84e436257730c23f1c1768ceb2
M.Gholamizadeh <pranceoffire50@gmail.com>
2024-02-16 19:45:01 +0330

    add logo to project properly

--- DotNetReportsEngine.ReadmeGeneration/DotnetReportsEngine.SampleCode/Program.cs
+++ DotNetReportsEngine.ReadmeGeneration/DotnetReportsEngine.SampleCode/Program.cs
@@ -2,12 +2,10 @@
 using DotNetReportsEngine.ReadmeGeneration;
 using DotNetReportsEngine.ReadmeGeneration.Details;
 
-var s = typeof(DotnetReportResourcesss).GetMethods().Where(_ => _.IsPublic && _.DeclaringType == typeof(DotnetReportResourcesss));
 var ReadmeGenerator =
     new ReadmeGenerator()
-        .AddAssembly(typeof(Program).Assembly)
-        .SetProjectDetail("Readme Generator Test", "detail")
-        .AddAcknowledgement("Awesome README", "https://github.com/matiassingers/awesome-readme")
+        .AddAssembly(typeof(ReadmeGenerator).Assembly)
+        .SetProjectDetail("Readme Generator ", "This Package Was Developed For Generating The Base Readme File To Improve The Speed Of Ccreating Standard Readme File and Make It Easy to All Developers To Creating Readme File Automatically . Enjoy It")
         .AddAppendixes(new List<Appendix>()
         {
             new Appendix()
@@ -19,11 +17,29 @@ var ReadmeGenerator =
                 DescriptionOrScript = "Hello"
             }
         })
-        .AddAuthor("Mohammadreza", "google.com")
-        .AddColor("Black", "00b48a")
-        .SetContributingDescription("This is Test For Text")
-        .AddDemoLink("")
-        .AddFAQ("Hello", "To You")
-        .AddFeatures("Hi1", "Hi2")
-        .RenderAllAssembliesToText();
+        .AddAuthor("@Mohammadreza Gholamizadeh [Phoenix]", "https://github.com/MohammadRezaGholamizadeh")
+        .SetGitHub_AboutMe("I Am Dotnet Software Developer That Always Try To Make All things Easy for Developing")
+        .AddGitHub_Links(new List<GitHubLink>()
+        {
+            new GitHubLink()
+            {
+                LogoType = LogoLinkType.Github,
+                Title = "GitHub",
+                Link = "https://github.com/MohammadRezaGholamizadeh",
+            },
+            new GitHubLink()
+            {
+                LogoType = LogoLinkType.Nuget,
+                Title = "Nuget",
+                Link = "https://www.nuget.org/profiles/MohammadrezaGholamizadeh_Phoenix"
+            }
+        })
+        .SetLicense(new License()
+        {
+            Title = "AGPL-3.0 license",
+            LicenseLink = "https://github.com/MohammadRezaGholamizadeh/DotnetReportsEngine.ReadmeGeneration?tab=AGPL-3.0-1-ov-file#"
+        })
+        .RenderAllAssembliesToText()
+        ;
+
 Console.Read();
